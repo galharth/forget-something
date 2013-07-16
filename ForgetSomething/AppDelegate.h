e7205568c5b83d9431be34fc7ed31eb394d3b028
Gal Harth <galharth960@gmail.com>
2013-07-16 16:25:13 +0300

    Initial Commit

---
+++ ForgetSomething/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ForgetSomething
+//
+//  Created by gal harth on 7/16/13.
+//  Copyright (c) 2013 Gal Harth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ForgetSomething/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ForgetSomething
+//
+//  Created by gal harth on 7/16/13.
+//  Copyright (c) 2013 Gal Harth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
